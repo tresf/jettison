6a056c58dd93d4953b08a8087a5a966817a9b081
tresf <tres.finocchiaro@gmail.com>
2019-11-12 13:00:43 -0500

    Fix indentFactor Remove `length() == 1` conditional; fix indentFactor Per https://github.com/jettison-json/jettison/issues/29

--- src/main/java/org/codehaus/jettison/json/JSONObject.java
+++ src/main/java/org/codehaus/jettison/json/JSONObject.java
@@ -1135,34 +1135,26 @@ public class JSONObject implements Serializable {
         Iterator     keys = keys();
         StringBuilder sb = new StringBuilder("{");
         int          newindent = indent + indentFactor;
-        Object       o;
-        if (n == 1) {
-            o = keys.next();
+
+        while (keys.hasNext()) {
+            Object o = keys.next();
+            if (sb.length() > 1) {
+                sb.append(",\n");
+            } else {
+                sb.append('\n');
+            }
+            for (i = 0; i < newindent; i += 1) {
+                sb.append(' ');
+            }
             sb.append(quote(o.toString()));
             sb.append(": ");
             sb.append(valueToString(this.myHashMap.get(o), indentFactor,
-                    indent));
-        } else {
-            while (keys.hasNext()) {
-                o = keys.next();
-                if (sb.length() > 1) {
-                    sb.append(",\n");
-                } else {
-                    sb.append('\n');
-                }
-                for (i = 0; i < newindent; i += 1) {
-                    sb.append(' ');
-                }
-                sb.append(quote(o.toString()));
-                sb.append(": ");
-                sb.append(valueToString(this.myHashMap.get(o), indentFactor,
-                        newindent));
-            }
-            if (sb.length() > 1) {
-                sb.append('\n');
-                for (i = 0; i < indent; i += 1) {
-                    sb.append(' ');
-                }
+                    newindent));
+        }
+        if (sb.length() > 1) {
+            sb.append('\n');
+            for (i = 0; i < indent; i += 1) {
+                sb.append(' ');
             }
         }
         sb.append('}');
